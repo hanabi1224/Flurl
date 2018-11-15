4613823cdeefa6e92ec0147e7ed772b2c07aa623
hanabi1224 <harlowmoo@gmail.com>
2018-11-15 15:21:16 +0800

    Implement HttpClientFactory for http/2 and mTLS

---
+++ src/Flurl.Http.Extras/Http2ClientFactory.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Http;
+using Flurl.Http.Configuration;
+
+namespace Flurl.Http
+{
+    public class Http2ClientFactory : DefaultHttpClientFactory
+    {
+        public override HttpMessageHandler CreateMessageHandler()
+        {
+            return new Http2MessageHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate,
+            };
+        }
+    }
+}
---
+++ src/Flurl.Http.Extras/MtlsHttpClientFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using Flurl.Http.Configuration;
+
+namespace Flurl.Http
+{
+    public class MtlsHttpClientFactory : DefaultHttpClientFactory
+    {
+        private readonly X509Certificate2 clientCertificate;
+        public MtlsHttpClientFactory(X509Certificate2 clientCertificate)
+        {
+            this.clientCertificate = clientCertificate ?? throw new ArgumentNullException(nameof(clientCertificate));
+        }
+
+        public override HttpMessageHandler CreateMessageHandler()
+        {
+            var handler = new HttpClientHandler
+            {
+                // #266
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+
+            handler.ClientCertificateOptions = ClientCertificateOption.Manual;
+            handler.ClientCertificates.Add(clientCertificate);
+
+            return handler;
+        }
+    }
+}
